ff133a36064e23df0a231ae8b89565b274309663
avnigulati05 <159441340+avnigulati05@users.noreply.github.com>
2024-03-18 17:38:33 +0530

    String
    character index
    string contain a character

---
+++ Umbrella.java
@@ -0,0 +1,18 @@
+public class Umbrella 
+{
+
+    public static void main (String args []){
+         String word = "Umbrella";
+         boolean isPresent  = word.contains("e");
+
+         if (isPresent){
+             System.out.println("letter e is present !");
+         }
+
+    
+
+    else {
+        System.out.println("letter e not found !");
+    }
+}
+}
